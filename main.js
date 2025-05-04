891b3da7c3f8968b8bba109af3a572a17d5d18d1
thevalter <valternunes776@gmail.com>
2025-05-03 23:38:37 -0300

    fix: video and shorts links already supported

--- main.js
+++ main.js
@@ -1,48 +1,51 @@
-document.querySelector('.submit').addEventListener('click', async () => {
+document.querySelector(".submit").addEventListener("click", async () => {
+  const videoId = document.querySelector("#input-field");
 
-    const videoId = document.querySelector('#input-field');
+  function youtube_parser(url) {
+    let regex =
+      /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
+    return regex.exec(url)[3];
+  }
 
-    function youtube_parser(url) {
-        let regex = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(.*?((?=[&#?])|$))/gm;
-        return regex.exec(url)[3];
-    }
+  const url = `https://youtube-video-fast-downloader-24-7.p.rapidapi.com/download_audio/${youtube_parser(
+    videoId.value
+  )}?quality=251`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "789b9f8786msha9f715d7770eab4p1cf63ajsn61ec18d193a1",
+      "X-RapidAPI-Host": "youtube-video-fast-downloader-24-7.p.rapidapi.com",
+    },
+  };
 
-    const url = `https://youtube-mp3-download1.p.rapidapi.com/dl?id=${youtube_parser(videoId.value)}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '944441af27mshe0218a31a0130c0p1dd4c5jsndcd813120fc7',
-            'X-RapidAPI-Host': 'youtube-mp3-download1.p.rapidapi.com'
-        }
-    };
+  try {
+    document.querySelector(
+      ".content"
+    ).innerHTML = `<div class="video"><p>Aguarde alguns instantes...</p></div>`;
+    const response = await fetch(url, options);
+    const result = await response.json();
 
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-
-        console.log(result);
-
-        if (result.status === 'ok') {
-            document.querySelector('.content').innerHTML = `
+    if (result.file) {
+      document.querySelector(".content").innerHTML = `
                 <div class="video">
-                    <p>${result.title}</p>
-                    <img src=${result.thumb} alt="">
-                    <a href=${result.link} target="blank">Download MP3</a>
+                <a href=${result.file} target="blank">Download MP3</a>
                 </div>
             `;
-        } else {
-            document.querySelector('.content').innerHTML = `
+    } else {
+      document.querySelector(".content").innerHTML = `
                 <div class="error">
                     <p>Url do video invalida ou serviço temporariamente indisponivel</p>
                 </div>
             `;
-        }
-
-    } catch (error) {
-        console.error(error);
     }
-
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-document.querySelector('.clear')
-    .addEventListener('click', () => document.querySelector('#input-field').value = "");
+document
+  .querySelector(".clear")
+  .addEventListener(
+    "click",
+    () => (document.querySelector("#input-field").value = "")
+  );
